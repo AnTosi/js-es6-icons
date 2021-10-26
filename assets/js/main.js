30abd60e21a104e507a1604a08537dfd8e27c4c8
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-26 16:09:50 +0200

    bloccato su card is not defined

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,7 +118,51 @@ let icons =
 	}
 ];
 
+var filter = document.getElementById("iconFilter").value;
+console.log(filter);
 
+// inzializzo icon
+
+icon = "";
 // Milestone 1
 // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
 //creo in html il layout, poi lo copio qua
+
+// Fatto html e css di base, ora ciclo per stamparle
+
+//uso foreach per due motivi, prima cosa devo fare un ciclo su un array e non mi interessa utilizzare i e array, seconda cosa Fabio aveva detto che oggi avremmo fatto un esercizio sugli ultimi cicli ò.ò
+
+//userò anche filter, metodo che abbiamo visto noi, e poi probabilmente userò if includes per adattare il filtro
+//ah, prima di tutto questo non ho settato l'option come variabile, che mi servirà per capire quale filtro adattare
+//i prefissi e la famiglia mi servono per comporre le classi che poi utilizza fontawsome per cercare l'icona, e dovrò combinarli con il name, mi pare che icona fontawsome sia tipo fas fa-user-secret, una cosa del genere, poi controllo
+
+// dicitura precisa fontawesome: <i class="fas fa-user-ninja"></i>
+//ovvero `${family} + " " + ${prefix} + ${name}
+// let card = `<div class="card text-center col-2 d-flex flex-column justify-content-center my-4 mx-3">
+// <div class="icon">
+//     <i><i class="${icon.family} ${icon.prefix}${icon.name}"></i></i>
+// </div>
+// <p class="text-center">
+//     CAT
+// </p>
+// </div>`
+
+
+
+
+icons.forEach(icon => {
+    generateCard(icon);
+    document.getElementById("container").insertAdjacentHTML("beforeend", card)
+    
+});
+
+function generateCard(icon,card) {
+    card = `<div class="card text-center col-2 d-flex flex-column justify-content-center my-4 mx-3">
+    <div class="icon">
+        <i><i class="${icon.family} ${icon.prefix}${icon.name}"></i></i>
+    </div>
+    <p class="text-center">
+        CAT
+    </p>
+    </div>`
+}
